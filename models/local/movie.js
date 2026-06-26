89953edc0908ab55e3f4922f0ede74355848ba12
JoseHbtC <jose.echavarria@hibotchat.com>
2026-06-26 16:05:01 -0500

    Update: movie local

--- models/local/movie.js
+++ models/local/movie.js
@@ -1,4 +1,4 @@
-import { readJson } from '../utils/require.js'
+import { readJson } from '../../utils/require.js'
 import { randomUUID } from 'node:crypto'
 
 const movies = readJson('../movies.json')
